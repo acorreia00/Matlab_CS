49c500a7f3da8b9aa3db28a900cee216357a5131
acorreia00 <afonsoscorreia@gmail.com>
2022-04-20 11:08:56 +0100

    Matlab Valores

---
+++ TP4/TP4_MatlabValores.m
@@ -0,0 +1,54 @@
+% Declaração de variáveis
+clear % limpa as variáveis da workspace
+clc % Limpa o ecrã de comandos do Matlab
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Mensagem inicial para a linha de comandos
+disp(' '); disp(' ');
+disp('ISEL / DEEEA - Controlo de Sistemas - Semestre Verão 2021/2022')
+disp(' ');
+disp('Nome do mfile: mf_dados_3Tanques')
+disp(' ');
+disp('Data última atualização (15-março-2022)')
+disp(' ');
+disp('Objetivo: Carregar as variáveis do trabalho nº 4 - Simulação de Sistemas (Controlo do Nível 3')
+disp(' '); disp(' '); disp(' '); disp(' '); disp(' '); disp(' ');
+
+
+%Parâmetros do Tanque 1
+R1=1; % Resistência Hidráulica do tanque 1 (s/m^2)
+C1=1; % Capacitância hidráulica do tanque 1 ou area do tanque 1 (m^2)
+
+
+%Parâmetros do Tanque 2
+R2=1; % Resistência Hidráulica do tanque 2 (s/m^2)
+C2=1; % Capacitância hidráulica do tanque 2 ou área do tanque 2 (m^2)
+
+
+%Parâmetros do Tanque 3
+R3=1; % Resistência Hidráulica do tanque 3 (s/m^2)
+C3=1; % Capacitância hidráulica do tanque 3 ou área do tanque 3 (m^2)
+
+
+% Potenciómetro de entrada
+KP_in=1; % Ganho do potenciómetro de entrada (V/m)
+
+
+% Sensor de Nível
+KS=1; % Ganho do sensor de nível (V/m)
+
+
+% Motor + Redutor + Válvula
+KMRV=1; % Ganho do Motor + Redutor + Válvula (m^3/(s V))
+
+
+% Parâmetros do controlador PID
+TC1=0.5;
+TC2=0.5;
+KC=2;
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Fim da Mensagem inicial para a linha de comandos
+disp('Variáveis do Trabalho 4 carregadas para a workspace:')
+who
+disp(' '); disp(' '); disp(' ');
